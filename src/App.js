fa577823197a4253b3ff583aa9066b607f8a06ec
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-06 15:46:02 +0100

    made type filter dynamic

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
   const [coordinates, setCoordinates] = useState();
   const [bounds, setBounds] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
   useEffect(() => {
     // Set default pos in NY in case the users deny geolocation
@@ -28,12 +30,12 @@ const App = () => {
   useEffect(() => {
     setIsLoading(true)
 
-    getPlacesData(bounds.sw, bounds.ne)
+    getPlacesData(type, bounds.sw, bounds.ne)
       .then((data) => {
         setPlaces(data);
         setIsLoading(false)
     })
-  }, [coordinates, bounds])
+  }, [type, coordinates, bounds])
   
   return (
     <>
@@ -45,6 +47,10 @@ const App = () => {
             places={places}
             childClicked={childClicked}
             isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
           />
         </Grid>
         <Grid item xs={12} md={8}>
